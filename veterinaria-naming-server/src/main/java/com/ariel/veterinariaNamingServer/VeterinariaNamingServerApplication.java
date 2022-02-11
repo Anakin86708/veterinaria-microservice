3b3ea08841b1fe7fd1900709ea62c900aa9dad96
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-02-11 08:36:59 -0300

    Initial naming server

---
+++ veterinaria-naming-server/src/main/java/com/ariel/veterinariaNamingServer/VeterinariaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.ariel.veterinariaNamingServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaNamingServerApplication.class, args);
+	}
+
+}
---
+++ veterinaria-naming-server/src/test/java/com/ariel/veterinariaNamingServer/VeterinariaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ariel.veterinariaNamingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
