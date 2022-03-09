494a1a41f24ca13ad51be5b8acf1f3ad63fe9314
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-03-09 12:52:06 -0300

    Unit tests for EspecieService.

--- especie-service/src/main/java/com/ariel/especieService/models/Especie.java
+++ especie-service/src/main/java/com/ariel/especieService/models/Especie.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
 @Entity
 public class Especie {
@@ -23,6 +24,11 @@ public class Especie {
         this.nome = nome;
     }
 
+    public Especie(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
     public Long getId() {
         return id;
     }
@@ -33,5 +39,18 @@ public class Especie {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Especie especie = (Especie) o;
+        return Objects.equals(id, especie.id) && Objects.equals(nome, especie.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
     }
 }
